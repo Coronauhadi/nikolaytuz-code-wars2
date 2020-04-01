dff0a8d8898c077c6eebd52a8c9b6454fdaefe8e
nikolaytuz <nikolaytuz56@gmail.com>
2020-04-01 19:57:49 +0400

    Добавил в components второй лвл

---
+++ src/views/gamel.vue
@@ -0,0 +1,34 @@
+<template >
+  <div class="gams">
+    <level :level="level"   />
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import level from '@/components/level.vue'
+
+export default {
+  name: 'gamel',
+  components: {
+    level
+  },
+  data: ()=>{
+    return {
+      level: 1,
+      win: false,
+      level1:{
+        
+      },
+    }
+  },
+  methods: {
+    win: function(){
+      level++
+    },
+  },
+}
+
+
+</script>
